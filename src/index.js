469aba06b051d92a6346aa06acb49a2d3846b8a8
ValeryKulakov <kulakov.resurs@tut.by>
2021-09-22 19:37:51 +0300

    Add Create-React-App tool.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import reportWebVitals from './reportWebVitals';
+
 class Button extends React.Component {
     constructor(props) {
         super(props);
@@ -43,3 +47,8 @@ ReactDOM.render(
     <App />,
     document.getElementById('app-root'),
 );
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
